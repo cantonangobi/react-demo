69877cd513e3d478f376d0acdc37248ba3e38af8
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-18 00:07:11 +0300

    passing children

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 // import Message from './Message'
 
 function App() {
-  let items = ["Nairobi", "Mombasa", "Nakuru", "Naivasha", "Ngong"];
+  // let items = ["Nairobi", "Mombasa", "Nakuru", "Naivasha", "Ngong"];
 
-  const handleSelectitem = (item: string) => {
-    console.log(item);
-  }
+  // const handleSelectitem = (item: string) => {
+  //   console.log(item);
+  // }
   return (
     <div>
-      <ListGroup  items={items} heading = "Cities" onSelectItem={handleSelectitem}/>
+      <Alert>
+        Hello, <span>World</span>
+      </Alert>
     </div>
   );
 }
