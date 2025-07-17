da170e352f8778ea2ecab98550f9a1956583c39e
Cantona Ngobi <cantonangobi@gmail.com>
2025-07-17 23:03:06 +0300

    handling events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,31 @@
 // import { Fragment } from "react/jsx-runtime";
-
+import { MouseEvent } from "react";
 function ListGroup() {
-  const items = ["Nairobi", "Mombasa", "Nakuru", "Naivasha", "Ngong"];
+  let items = ["Nairobi", "Mombasa", "Nakuru", "Naivasha", "Ngong"];
+  // items = [];
+  const handleClick = (event: MouseEvent) => console.log(event);
+  // items.map((item) => <li>{item}</li>);
 
-  items.map((item) => <li>{item}</li>);
+  // if (items.length === 0) {
+  //   return (
+  //     <>
+  //       <h1>List</h1>
+  //       <p>No item Found</p>
+  //     </>
+  //   );
+  // }
 
+  // const message = items.length === 0 ? <p> No item found</p> : null
   return (
     <>
       <h1>List</h1>
+      {/* {message} */}
+      {items.length === 0 && <p>No item Found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item} className="list-group-item">{item}</li>
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
+            {item}
+          </li>
         ))}
         {/* <li className="list-group-item">An item</li>
                 <li className="list-group-item">A second item</li>
